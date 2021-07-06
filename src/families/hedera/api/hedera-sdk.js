2f683ad4da09d047277cf7b7a12adef931cc65bb
jackthta <jack.th.ta@gmail.com>
2021-07-06 14:22:07 -0700

    Hedera integration
    Add Hedera App JS bindings
    
    Add hw-getAddress.js file to derive address from device
    
    Add types.js for hedera-specific types
    
    Add transaction.js
    
    Add Hedera API wrapping
    
    Add JS Bridge
    
    Add js-synchronisation.js
    
    Add js-transaction.js
    
    Add js-getTransactionStatus.js
    
    Deal with logic and errors
    
    Add logic.js
    
    Add js-buildTransaction.js
    
    Add js-signOperation.js
    
    Add js-broadcast.js
    
    Add js-estimateMaxSpendable.js
    
    Add js-getFeesForTransaction.js
    
    Add cli-transaction.js
    
    Add preload.js
    
    Add getDeviceTransactionConfig.js
    
    Add test-dataset.js
    
    Generate hedera-specific imports
    
    Remove all instances of nonce

---
+++ src/families/hedera/api/hedera-sdk.js
@@ -0,0 +1,9 @@
+// @flow
+// TODO: Fill in functions
+
+export const getAccount = () => ({});
+export const getOperations = () => ({});
+export const getPreloadedData = () => ({});
+export const getFees = () => ({});
+export const submit = () => ({});
+export const disconnect = () => ({});
